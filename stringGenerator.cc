e5a351532647da7fc8f095e96e8bf46d99050e0f
Ordencia Wu <ordencia525@gmail.com>
2019-11-27 01:05:52 -0500

    First commit for stringGenerator

---
+++ stringGenerator.cc
@@ -0,0 +1,89 @@
+#include "stringGenerator.h"
+#include "state.h"
+using namespace std;
+
+stringGenerator::stringGenerator() {
+	vector<vector<char>> newGrid;
+
+	for (int i = 0; i < gridRows; ++i) {
+		vector<char> row;
+		newGrid.emplace_back(row);
+		for (int j = 0; j < gridCols; ++j) {
+			newGrid[i].emplace_back(' ');
+		}
+	}
+
+	grid = newGrid;
+}
+
+void stringGenerator::notify(Subject &whoNotified) { // update the display for one cell
+	State st = whoNotified.getState();
+	if (st.add) {
+		grid[st.coord.x][st.coord.y] = st.type;
+	} else {
+		grid[st.coord.x][st.coord.y] = ' ';
+	}
+}
+
+string TextDisplay::print(int level, int score, bool blind, char next) { // generate a string that represents one board
+	string str = "";
+
+	str += "Level:    " + level + "\n";
+	str += "Score:    " + score + "\n";
+
+	for (int i = 0; i < gridCols; ++i) {
+		str += "-";
+	}
+	str += "\n";
+
+	// does row and col start from 0? 
+	for (int i = 0; i < gridRows; ++i) {
+		for (int j = 0; j < gridCols; ++j) {
+			if (blind) {
+				if ((i >= 3 && i <= 12) || (j >= 3 && j <= 9)) {
+					str += "?";
+				}
+			} else {
+				str += "" + grid[i][j];
+			}
+		}
+		str += "\n";
+	}
+
+	for (int i = 0; i < gridCols; ++i) {
+		str += "-";
+	}
+	str += "\n";
+
+	str += "Next:\n";
+
+	if (next != null) {
+		switch (next) {
+			case 'I': 
+				str += "           \nIIII       \n";
+				break;
+			case 'J':
+				str += "J          \nJJJ        \n";
+				break;
+			case 'L':
+				str += "  L        \nLLL        \n";
+				break;
+			case 'O':
+				str += "OO         \nOO         \n";
+				break;
+			case 'S':
+				str += " SS        \nSS         \n";
+				break;
+			case 'Z':
+				str += "ZZ         \n ZZ        \n";
+				break;
+			case 'T':
+				str += "TTT        \n T         \n";
+				break;
+		}
+	} else {
+		str += "           \n           \n";
+	}
+
+	return str;
+}
---
+++ stringGenerator.h
@@ -0,0 +1,19 @@
+#ifndef _STRINGGENERATOR_H_
+#define _STRINGGENERATOR_H_
+
+class Cell;
+
+class StringGenerator: public Observer {
+  std::vector<std::vector<char>> grid;
+  const int gridRows = 18;
+  const int gridCols = 11;
+
+ public:
+  StringGenerator();
+
+  void notify(Subject &whoNotified) override;
+
+  std::string print(int level, int score, bool blind, char next);
+};
+
+#endif
