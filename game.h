364e1231ea869d14bc4d32d194fe699f3d471876
mandymcao <mdcao@edu.uwaterloo.ca>
2019-11-27 14:10:30 -0500

    Create game.h

---
+++ game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+#include <string>
+#include <memory>
+#include <iostream>
+
+class Board;
+class TextDisplay;
+class Game {
+	const rows = 18;
+	int highscore = 0;
+	std::unique_ptr<Board> brd1 {new Board()};
+	std::unique_ptr<Board> brd2 {new Board()};
+	std::unique_ptr<TextDisplay> td {new TextDisplay()};
+public:
+	Game(int startLevel = 0);
+	void restart();
+	void print(std::ostream &out);
+	void processCommand(std::string command, int repeat, int board);
+	void processCommand(std::string command, std::string filename, int repeat, int board)
+};
+
+#endif
