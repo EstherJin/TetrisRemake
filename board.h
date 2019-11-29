f1702120749015cf7778ca3285a8ac21a1f4d65e
mandymcao <mdcao@edu.uwaterloo.ca>
2019-11-28 19:55:35 -0500

    Update board.h

--- board.h
+++ board.h
@@ -14,20 +14,20 @@ class Row;
 
 class Board {
 	const gridRows = 18;
+	StringGenerator sg;
 	std::vector<Row> grid;
 	std::vector<Block2> activeBlocks;
 	int score = 0;
-	Level lvl;
+	std::unique_ptr<Level> lvl;
 	std::unique_ptr<Block1> currentBlock;
 	std::unique_ptr<Block1> nextBlock;
-	StringGenerator sg;
 public:
 	Board(int level = 0);
 	virtual ~Board() = 0;
-	std::string print(bool blind);
+	virtual std::string print(bool blind);
 	void turnBlock(int amount);
-	void moveBlock(int amount);
-	void downBlock(int amount);
+	virtual void moveBlock(int amount);
+	virtual void downBlock(int amount);
 	int dropBlock(); // returns number of lines cleared
 	void changeLevel(int direction, bool random, std::string filename);
 	int getScore();
