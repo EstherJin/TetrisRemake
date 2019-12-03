8892341adf804c06baa006267d4ecc63c5129f4d
Ordencia Wu <ordencia525@gmail.com>
2019-12-03 15:53:06 -0500

    Updated graphicsDisplay.cc grid lines

--- graphicsDisplay.cc
+++ graphicsDisplay.cc
@@ -15,18 +15,19 @@ GraphicsDisplay::GraphicsDisplay ()  {
 
     window = make_unique<Xwindow> ();
 
-    window->fillRectangle(0,0,405,817,0);
+    window->fillRectangle(0,0,405,780,0);
     window->fillRectangle(0,50,405,642,9);
-    window->fillRectangle(15,75,375,612,0);
+    window->fillRectangle(15,65,375,612,0);
     for (int i = 1; i < row; i++) {
-        window->fillRectangle(15,75+i*34,375,1,10);
+        window->fillRectangle(xmar,ymar+i*34,375,1,10);
     }
     for (int j = 1; j < col; j++) {
-        window->fillRectangle(15+j*34,75,1,612,10);
+        window->fillRectangle(xmar+j*34,ymar,1,612,10);
     }
+    // window->fillRectangle(0, 0, 100, 100, 4);
 
     string nt = "Next:";
-    drawString (50, 692, nt);
+    window->drawString (15, 700, nt);
 }
 
 void GraphicsDisplay::notify (Subject &whoNotified) {
@@ -34,38 +35,45 @@ void GraphicsDisplay::notify (Subject &whoNotified) {
 
     if (st.add) {
         if (st.type == 'T') {
-            window->fillRectangle(15 + st.coords.col * cellLength,
-                                  75 + st.coords.row * cellLength,
+            window->fillRectangle(xmar + st.coords.col * cellLength,
+                                  ymar + st.coords.row * cellLength,
                                   cellLength, cellLength, 2);
         } else if (st.type == 'L') {
-            window->fillRectangle(15 + st.coords.col * cellLength,
-                                  75 + st.coords.row * cellLength,
+            window->fillRectangle(xmar + st.coords.col * cellLength,
+                                  ymar + st.coords.row * cellLength,
                                   cellLength, cellLength, 3);
         } else if (st.type == 'Z') {
-            window->fillRectangle(15 + st.coords.col * cellLength,
-                                  75 + st.coords.row * cellLength,
+            window->fillRectangle(xmar + st.coords.col * cellLength,
+                                  ymar + st.coords.row * cellLength,
                                   cellLength, cellLength, 4);
         } else if (st.type == 'O') {
-            window->fillRectangle(15 + st.coords.col * cellLength,
-                                  75 + st.coords.row * cellLength,
+            window->fillRectangle(xmar + st.coords.col * cellLength,
+                                  ymar + st.coords.row * cellLength,
                                   cellLength, cellLength, 5);
         } else if (st.type == 'S') {
-            window->fillRectangle(15 + st.coords.col * cellLength,
-                                  75 + st.coords.row * cellLength,
+            window->fillRectangle(xmar + st.coords.col * cellLength,
+                                  ymar + st.coords.row * cellLength,
                                   cellLength, cellLength, 6);
         } else if (st.type == 'J') {
-            window->fillRectangle(15 + st.coords.col * cellLength,
-                                  75 + st.coords.row * cellLength,
+            window->fillRectangle(xmar + st.coords.col * cellLength,
+                                  ymar + st.coords.row * cellLength,
                                   cellLength, cellLength, 7);
         } else if (st.type == 'I') {
-            window->fillRectangle(15 + st.coords.col * cellLength,
-                                  75 + st.coords.row * cellLength,
+            window->fillRectangle(xmar + st.coords.col * cellLength,
+                                  ymar + st.coords.row * cellLength,
                                   cellLength, cellLength, 8);
         }
     } else {
-        window->fillRectangle(15 + st.coords.col * cellLength,
-                              75 + st.coords.row * cellLength,
-                              cellLength, cellLength, 0)
+        window->fillRectangle(xmar + st.coords.col * cellLength,
+                              ymar + st.coords.row * cellLength,
+                              cellLength, cellLength, 0);
+    }
+    for (int i = 1; i < row; i++) {
+
+        window->fillRectangle(xmar,ymar+i*34,375,1,10);
+    }
+    for (int j = 1; j < col; j++) {
+        window->fillRectangle(xmar+j*34,ymar,1,612,10);
     }
 
 
@@ -75,7 +83,7 @@ void GraphicsDisplay::printLevel(int level) {
     window->fillRectangle(0,0,405,1,0);
     string lvl = "Level:                    ";
     lvl += to_string(level);
-    window->drawString(50, 15, lvl);
+    window->drawString(50, xmar, lvl);
 }
 
 void GraphicsDisplay::printScore(int score) {
@@ -89,40 +97,40 @@ void GraphicsDisplay::printNext (char next) {
     if (next != ' ') {
         switch (next) {
             case 'T':
-                window->fillRectangle(15,712, cellLength, cellLength, 2);
-                window->fillRectangle(15+cellLength,712, cellLength, cellLength, 2);
-                window->fillRectangle(15+2*cellLength,712, cellLength, cellLength, 2);
-                window->fillRectangle(15+cellLength,712+cellLength, cellLength, cellLength, 2);
+                window->fillRectangle(xmar,712, cellLength, cellLength, 2);
+                window->fillRectangle(xmar+cellLength,712, cellLength, cellLength, 2);
+                window->fillRectangle(xmar+2*cellLength,712, cellLength, cellLength, 2);
+                window->fillRectangle(xmar+cellLength,712+cellLength, cellLength, cellLength, 2);
             case 'L':
-                window->fillRectangle(15+2*cellLength,712, cellLength, cellLength, 3);
-                window->fillRectangle(15,712+cellLength, cellLength, cellLength, 3);
-                window->fillRectangle(15+cellLength,712+cellLength, cellLength, cellLength, 3);
-                window->fillRectangle(15+2*cellLength,712+cellLength, cellLength, cellLength, 3);
+                window->fillRectangle(xmar+2*cellLength,712, cellLength, cellLength, 3);
+                window->fillRectangle(xmar,712+cellLength, cellLength, cellLength, 3);
+                window->fillRectangle(xmar+cellLength,712+cellLength, cellLength, cellLength, 3);
+                window->fillRectangle(xmar+2*cellLength,712+cellLength, cellLength, cellLength, 3);
             case 'Z':
-                window->fillRectangle(15,712, cellLength, cellLength, 4);
-                window->fillRectangle(15+cellLength,712, cellLength, cellLength, 4);
-                window->fillRectangle(15+cellLength,712+cellLength, cellLength, cellLength, 4);
-                window->fillRectangle(15+2*cellLength,712+cellLength, cellLength, cellLength, 4);
+                window->fillRectangle(xmar,712, cellLength, cellLength, 4);
+                window->fillRectangle(xmar+cellLength,712, cellLength, cellLength, 4);
+                window->fillRectangle(xmar+cellLength,712+cellLength, cellLength, cellLength, 4);
+                window->fillRectangle(xmar+2*cellLength,712+cellLength, cellLength, cellLength, 4);
             case 'O':
-                window->fillRectangle(15,712, cellLength, cellLength, 5);
-                window->fillRectangle(15+cellLength,722,cellLength, cellLength, 5);
-                window->fillRectangle(15,712+cellLength, cellLength, cellLength, 5);
-                window->fillRectangle(15+cellLength,712+cellLength, cellLength, cellLength, 5);
+                window->fillRectangle(xmar,712, cellLength, cellLength, 5);
+                window->fillRectangle(xmar+cellLength,722,cellLength, cellLength, 5);
+                window->fillRectangle(xmar,712+cellLength, cellLength, cellLength, 5);
+                window->fillRectangle(xmar+cellLength,712+cellLength, cellLength, cellLength, 5);
             case 'S':
-                window->fillRectangle(15+cellLength,712, cellLength, cellLength, 6);
-                window->fillRectangle(15+2*cellLength,712, cellLength, cellLength, 6);
-                window->fillRectangle(15,712+cellLength, cellLength, cellLength, 6);
-                window->fillRectangle(15+cellLength,712+cellLength, cellLength, cellLength, 6);
+                window->fillRectangle(xmar+cellLength,712, cellLength, cellLength, 6);
+                window->fillRectangle(xmar+2*cellLength,712, cellLength, cellLength, 6);
+                window->fillRectangle(xmar,712+cellLength, cellLength, cellLength, 6);
+                window->fillRectangle(xmar+cellLength,712+cellLength, cellLength, cellLength, 6);
             case 'J':
-                window->fillRectangle(15,712, cellLength, cellLength, 7);
-                window->fillRectangle(15,712+cellLength, cellLength, cellLength, 7);
-                window->fillRectangle(15+cellLength,712+cellLength, cellLength, cellLength, 7);
-                window->fillRectangle(15+2*cellLength,712+cellLength, cellLength, cellLength, 7);
+                window->fillRectangle(xmar,712, cellLength, cellLength, 7);
+                window->fillRectangle(xmar,712+cellLength, cellLength, cellLength, 7);
+                window->fillRectangle(xmar+cellLength,712+cellLength, cellLength, cellLength, 7);
+                window->fillRectangle(xmar+2*cellLength,712+cellLength, cellLength, cellLength, 7);
             case 'I':
-                window->fillRectangle(15,712+cellLength, cellLength, cellLength, 8);
-                window->fillRectangle(15+cellLength,712+cellLength, cellLength, cellLength, 8);
-                window->fillRectangle(15+2*cellLength,712+cellLength, cellLength, cellLength, 8);
-                window->fillRectangle(15+3*cellLength,712+cellLength, cellLength, cellLength, 8);
+                window->fillRectangle(xmar,712+cellLength, cellLength, cellLength, 8);
+                window->fillRectangle(xmar+cellLength,712+cellLength, cellLength, cellLength, 8);
+                window->fillRectangle(xmar+2*cellLength,712+cellLength, cellLength, cellLength, 8);
+                window->fillRectangle(xmar+3*cellLength,712+cellLength, cellLength, cellLength, 8);
         }
     }
 }
